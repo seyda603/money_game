4a5c327972a0cedf9c1ad1ca4c721ff4867057e9
seyda603 <selcanseydaaydin@gmail.com>
2021-09-21 12:16:38 +0300

    my first commit

--- src/test/java/com/moneygame/test/test1.java
+++ src/test/java/com/moneygame/test/test1.java
@@ -1,5 +1,16 @@
 package com.moneygame.test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class test1 {
 
+    @Test
+    public void test2(){
+    }
+    @AfterMethod
+    public void tearDown(){
+
+    }
 }
