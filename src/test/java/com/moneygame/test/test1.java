080b80bad70ab4d566ddc79acceec0ff60d2954c
seyda603 <selcanseydaaydin@gmail.com>
2021-09-21 12:34:44 +0300

    I added my webdriverfactory

--- src/test/java/com/moneygame/test/test1.java
+++ src/test/java/com/moneygame/test/test1.java
@@ -1,11 +1,15 @@
 package com.moneygame.test;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class test1 {
+@BeforeMethod
+public void a(){
 
+}
     @Test
     public void test2(){
     }
